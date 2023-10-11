b75d206576afce6fe4e53ecd27badfb0d4118fff
Giulia_vi95 <giulia.vaccaro95@gmail.com>
2023-10-11 15:46:15 +0200

    new file

---
+++ index.js
@@ -0,0 +1,16 @@
+// Question.1 - Cos'è un Algoritmo? Spiegalo a parole tue:
+// Ask.1 - Un algoritmo è un insieme di passaggi fondamentali ed utili per il raggiungimento di un obiettivo;
+// Esempio: Se un amico mi chiedesse come si crea una campagna social, e io gli spiegassi tutti i passaggi, 
+//dal Briefing con il cliente, al brainstorming in agenzia, creazione del concept, 
+//shooting fotografico, creazione delle grafiche personalizzate, studio e definizione del copy 
+//insieme al copywriter ed infine pubblicazione della campagna social, questo è proprio un algoritmo.
+
+// Question.2 - Cos'è una variabile? Spiegalo a parole tue:
+// Ask.2 - Una variabile, sempre in ambito campagne social, si può associare ai singoli passaggi 
+//utili alla creazione della campagna: per esempio una variabile può essere il brainstorming:
+//quel momento in cui tutti i componenti del team marketing buttano giù una serie di idee utili 
+//al raggiungimento dell'obiettivo.
+
+// Question.3 - Undefined e null sono la stessa cosa?
+// Ask.3 - Non sono la stessa cosa: undefined è una variabile il cui contenuto è vuoto, non definito;
+// null è invece una keyword che si utilizza per azzerare il contenuto della variabile.
